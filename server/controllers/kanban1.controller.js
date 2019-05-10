007ca73c29fe6f2c5799eb7715b676318454aee2
matuzalema <matuzalema@gmail.com>
2019-05-11 00:09:36 +0200

    Add components files

---
+++ server/controllers/kanban1.controller.js
@@ -0,0 +1,5 @@
+import Kanban1 from '../models/kanban1';
+
+export function getSomething(req, res) {
+  return res.status(200).end();
+}
---
+++ server/routes/kanban1.routes.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import * as Kanban1Controller from '../controllers/kanban1.controller';
+
+const router = new Router();
+
+
+export default router;

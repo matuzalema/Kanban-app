ad0cfd688b23766bea745b1eb599655b0887c7cd
matuzalema <matuzalema@gmail.com>
2019-05-28 13:52:35 +0200

    Add endpoints

--- server/routes/note.routes.js
+++ server/routes/note.routes.js
@@ -3,5 +3,13 @@ import * as NoteController from '../controllers/note.controller';
 
 const router = new Router();
 
+// Add a new Note
+router.route('/notes').post(NoteController.addNote);
+
+// Delete a note by noteId
+router.route('/notes/:noteId').delete(NoteController.deleteNote);
+
+// Edit a note by noteId
+router.route('/notes/:noteId').put(NoteController.editNote);
 
 export default router;
